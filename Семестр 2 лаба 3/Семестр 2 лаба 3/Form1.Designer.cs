bb40dc75f1ac850eb2dc69b2b0ed6efba4d7a787
Gomer1ch <yevhen.oshchepkov@nure.ua>
2023-07-08 18:40:26 +0300

    addlab

---
+++ Семестр 2 лаба 3/Семестр 2 лаба 3/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Семестр_2_лаба_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1069, 645);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1107, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Рисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1104, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1107, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Отрисовка по траектории";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1107, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Очистка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress1);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(31, 91);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Прямая";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(31, 114);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Парабола";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(1076, 184);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 187);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "10";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(31, 138);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Уменьшение";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(31, 162);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Увеличение";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 658);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPress1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Семестр 2 лаба 3/Семестр 2 лаба 3/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Семестр_2_лаба_3
+{
+    public partial class Form1 : Form
+    {
+        class House
+        {
+            public int X = 0;
+            public int Y = 0;
+            public float K = 1;
+            public int Angle = 0;
+            public House(int x, int y, float k, int angle)
+            {
+                X = x;
+                Y = y;
+                K = k;
+                Angle = angle;
+            }
+            //задаём координаты и размер дома
+        }
+        Bitmap bmp;
+        Graphics graph;
+        Pen pen;
+        House house;
+        int quantity = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graph = Graphics.FromImage(bmp);
+            pen = new Pen(Color.Black);
+            house = new House(pictureBox1.Width / 2, pictureBox1.Height / 2, 1, 0);
+        }
+        void DrawHouse()
+        {
+            //graph.Clear(pictureBox1.BackColor);
+            graph.DrawRectangle(pen, house.X, house.Y, 100 * house.K, 100 * house.K);
+            graph.DrawEllipse(pen, house.X + 35 * house.K, house.Y + 10 * house.K, 30 * house.K, 30 * house.K);
+            graph.DrawLine(pen, house.X + 50 * house.K, house.Y + 10 * house.K, house.X + 50 * house.K, house.Y + 40 * house.K);
+            graph.DrawLine(pen, house.X + 35 * house.K, house.Y + 25 * house.K, house.X + 65 * house.K, house.Y + 25 * house.K);
+            Point point1 = new Point(Convert.ToInt32(house.X - 5 * house.K), Convert.ToInt32(house.Y + 3 * house.K));
+            Point point2 = new Point(Convert.ToInt32(house.X + 30 * house.K), Convert.ToInt32(house.Y - 20 * house.K));
+            Point point3 = new Point(Convert.ToInt32(house.X + 50 * house.K), Convert.ToInt32(house.Y - 50 * house.K));
+            Point[] pts = new Point[3] { point1, point2, point3 };
+            graph.DrawCurve(pen, pts, 1);
+            Point point4 = new Point(Convert.ToInt32(house.X + 105 * house.K), Convert.ToInt32(house.Y + 3 * house.K));
+            Point point5 = new Point(Convert.ToInt32(house.X + 70 * house.K), Convert.ToInt32(house.Y - 20 * house.K));
+            Point[] pm = new Point[3] { point4, point5, point3 };
+            graph.DrawCurve(pen, pm, 1);
+            //Point point11 = new Point(Convert.ToInt32(house.X + 30 * house.K), Convert.ToInt32(house.Y + 100 * house.K));
+            //Point point12 = new Point(Convert.ToInt32(house.X + 35 * house.K), Convert.ToInt32(house.Y + 65 * house.K));
+            //Point point13 = new Point(Convert.ToInt32(house.X + 65 * house.K), Convert.ToInt32(house.Y + 65 * house.K));
+            //Point point14 = new Point(Convert.ToInt32(house.X + 70 * house.K), Convert.ToInt32(house.Y + 100 * house.K));
+            //Point[] pts2 = new Point[4] { point11, point12, point13, point14 };
+            //graph.DrawCurve(pen, pts2, 3);
+            Point start = new Point(Convert.ToInt32(house.X + 30 * house.K), Convert.ToInt32(house.Y + 100 * house.K));
+            Point control1 = new Point(Convert.ToInt32(house.X + 30 * house.K), Convert.ToInt32(house.Y + 50 * house.K));
+            Point control2 = new Point(Convert.ToInt32(house.X + 70 * house.K), Convert.ToInt32(house.Y + 50 * house.K));
+            Point end = new Point(Convert.ToInt32(house.X + 70 * house.K), Convert.ToInt32(house.Y + 100 * house.K));
+            Point[] bezierPoints = {start, control1, control2, end};
+            graph.DrawBeziers(pen, bezierPoints);
+            pictureBox1.Image = bmp;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graph.Clear(pictureBox1.BackColor);
+            DrawHouse();
+        }
+
+        private void KeyPress1(object sender, KeyPressEventArgs e)
+        {
+            label1.Text = "" + e.KeyChar;
+            if (e.KeyChar == 'd')
+            {
+                house.X += 10;
+                graph.Clear(pictureBox1.BackColor);
+                DrawHouse();
+            }
+            if (e.KeyChar == 'w')
+            {
+                house.Y -= 10;
+                graph.Clear(pictureBox1.BackColor);
+                DrawHouse();
+            }
+            if (e.KeyChar == 's')
+            {
+                house.Y += 10;
+                graph.Clear(pictureBox1.BackColor);
+                DrawHouse();
+            }
+            if (e.KeyChar == 'a')
+            {
+                house.X -= 10;
+                graph.Clear(pictureBox1.BackColor);
+                DrawHouse();
+            }
+
+            char number = e.KeyChar;
+            if (!(Char.IsDigit(number) || number == 8 || number == 44))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                house.K = Convert.ToSingle(textBox1.Text);
+                quantity = Convert.ToInt32(textBox2.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float t = house.K;
+            int n = 0;
+            if (radioButton1.Checked) n = 1;
+            if (radioButton2.Checked) n = 2;
+            int x0 = Convert.ToInt32(pictureBox1.Width) - 110;
+            int y0 = Convert.ToInt32(pictureBox1.Height) - 110;
+            graph.Clear(pictureBox1.BackColor);
+            switch (n)
+            {
+                case 1:
+                    {
+                        //прямая y=x
+                        for (int i = 0; i < quantity; i++)
+                        {
+                            house = new House(x0, y0, house.K, 0);
+                            DrawHouse();
+                            if (checkBox1.Checked)
+                            {
+                                house.K *= 0.7F;
+                            }
+                            if (checkBox2.Checked)
+                            {
+                                house.K /= 0.7F;
+                            }
+                            x0 -= Convert.ToInt32(100 * house.K);
+                            y0 -= Convert.ToInt32(100 * house.K);
+                            house = new House(pictureBox1.Width / 2, pictureBox1.Height / 2, house.K, 0);
+                        }
+                        house.K = t;
+                        
+                    }
+                    break;
+                case 2:
+                    {
+                        //парабола y=b+k(x-a)^2
+                        x0 = 0;
+                        for (int i = 0; i < quantity; i++)
+                        {
+                            y0 = 50 + (x0 - Convert.ToInt32(pictureBox1.Width) / 2) * (x0 - Convert.ToInt32(pictureBox1.Width) / 2) / 500;
+                            house = new House(x0, y0, house.K, 0);
+                            DrawHouse();
+                            if (checkBox1.Checked)
+                            {
+                                house.K *= 0.8F;
+                            }
+                            if (checkBox2.Checked)
+                            {
+                                house.K /= 0.8F;
+                            }
+                            x0 = Convert.ToInt32(x0 + 100*house.K);
+                            house = new House(pictureBox1.Width / 2, pictureBox1.Height / 2, house.K, 0);
+                        }
+                        house.K = t;
+                    }
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            graph.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = bmp;
+            house = new House(pictureBox1.Width / 2, pictureBox1.Height / 2, house.K, 0);
+        }
+    }
+}
